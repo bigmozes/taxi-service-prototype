2b842ec3009486b4df004435ad83bdfd9fb4338b
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-07-15 22:23:15 +0300

    Edited README.md file

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# taxi-service-prototype
+# Taxi Service Prototype
+## A prototype for Taxi Service Java Web Application
+### Features
+- Store data about Cars, Cars Manufacturers and Drivers
+- Bind multiple Drivers to a Car
+- Registering and authenticating as a Driver with ability to view info about 
+  current Driver's Cars
+- Deletion of Driver, Car or Manufacturer
+### How To Use
+- Taxi Service Prototype is a Tomcat-based Java Web Application.
+- Clone the repo.
+- Create Database using SQL scripts from resources/init_db.sql file.
+- Edit your database connection credentials in util/ConnectionUtil class. Replace YOUR DATABASE 
+  URL with URL like jdbc:mysql://localhost:3306/taxi.
+- JDBC driver, used in development:
+[com.mysql.cj.jdbc.Driver](https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-usagenotes-connect-drivermanager.html).
+- Compile and build running command `mvn clean package`, make sure the build is successful.
+- Now you can run the application directly from
+IDE ([IntelliJ IDEA](https://www.jetbrains.com/idea/) for example) with configured
+[Tomcat](https://tomcat.apache.org/).
+- To run locally or to deploy follow instructions on
+[Heroku](https://devcenter.heroku.com/articles/java-webapp-runner).
+### Technologies
+- Model-View-Controller Architectural Pattern.
+- Dependency Injection.
+- JDBC.
+- Java Web.
+- SQL.
+- JSP with HTML and JSTL markup.
+### Credits
+- [Bohdan Chupika](https://github.com/boroda4436)
+- [Daria Maliuk](https://github.com/MaliukDaria)
+- [Mykyta Arkhanhelskyi](https://github.com/Nick97-git)
+
---
+++ src/main/java/taxi/controller/driver/AddDriverController.java
@@ -0,0 +1,41 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Long userId = (Long) session.getAttribute("user_id");
+        if (userId == null) {
+            req.setAttribute("userLoggedIn", 0);
+        } else {
+            req.setAttribute("userLoggedIn", 1);
+        }
+        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name = req.getParameter("name");
+        String licenseNumber = req.getParameter("license_number");
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        Driver driver = new Driver(name, licenseNumber, login, password);
+        driverService.create(driver);
+        resp.sendRedirect(req.getContextPath() + "/drivers/add");
+    }
+}
